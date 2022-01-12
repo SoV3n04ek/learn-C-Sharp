4e4864536cb87dbbe48f03117bb1206784da00ef
Ivan Savin <68004563+SoV3n04ek@users.noreply.github.com>
2022-01-12 23:24:02 +0200

    homework2 first update

---
+++ homework2/ConsoleAplication/Book.cs
@@ -0,0 +1,105 @@
+﻿namespace Program
+{
+    class Author
+    {
+        private string name { get; set; }
+        private string lastName { get; set; }
+        private int birthdayYear { get; set; }
+
+        public Author(string _name, string _lastName, int _birthdayYear)
+        {
+            name         = _name;
+            lastName     = _lastName;
+            birthdayYear = _birthdayYear;
+        }
+        public override string ToString()
+        {
+            return "Name: " + name + "," +
+                " Last Name: " + lastName + ", " +
+                "birthday year: " + birthdayYear.ToString();
+        }
+    }
+
+    class Reader 
+    {
+        private string name { get; set; }
+        private string lastName { get; set; }
+
+        private int count;
+
+        private Book[] books;
+
+        public Reader(string _name, string _lastName, int countBooks, Book[] _books) 
+        {
+            name     = _name;
+            lastName = _lastName;
+            books    = _books;  
+            count    = countBooks;
+        }
+
+        public void Add(Book book)
+        {
+            Book[] tmpbooks = new Book[count + 1];
+
+            for (int i = 0; i < books.Length; i++)
+            {
+                tmpbooks[i] = books[i];
+            }
+
+            tmpbooks[tmpbooks.Length] = book;
+            count++;
+        }
+
+        public void Delete(Book book)
+        {
+            Book[] tmpbooks = new Book [count - 1];
+
+            for (int i = 0; i < books.Length; i++)
+            {
+                if (books[i] == book)
+                {
+                    continue;
+                }
+                tmpbooks[i] = books[i];
+            }
+
+            books = tmpbooks;
+            count--;
+        }
+
+        public void print()
+        {
+            for (int i = 0; i < count; i++)
+            {
+
+            }
+        }
+    }
+
+    class Book
+    {
+        private int id { get; set; }
+        private string name { get; set; }
+        private Author author { get; set; }
+        private string description { get; set; }
+        private int ISBN { get; set; }
+
+        public Book(int _id, string _name, Author _author, string _description, int _ISBN)
+        {
+            id          = id;
+            name        = name;
+            author      = author;
+            description = description;
+            ISBN        = ISBN;
+        }
+
+        public Book(Book other)
+        {
+            id          = other.id;
+            name        = other.name;
+            author      = other.author;
+            description = other.description;
+            ISBN        = other.ISBN;
+        }
+    }
+}  
---
+++ homework2/ConsoleAplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Program;
+
+namespace ConsoleAplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Author author = new Author("name", "surname", 1984);
+            Book a = new Book(1, "Name", author, "Description", 17849102);
+
+            int count = 10;
+            Book[] books = new Book[count];
+            Reader reader = new Reader("name", "surname", count, books);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
