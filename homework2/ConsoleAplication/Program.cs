c424786263b1041a5689ed422ce83d25fbfe07c2
Ivan Savin <68004563+SoV3n04ek@users.noreply.github.com>
2022-01-14 00:11:43 +0200

    fixed and tested static bool operator ==

--- homework2/ConsoleAplication/Program.cs
+++ homework2/ConsoleAplication/Program.cs
@@ -9,17 +9,19 @@ namespace ConsoleAplication
         {
             Author author = new Author("name", "surname", 1984);
             Book a = new Book(1, "Name", author, "Description", 17849102);
-            Book a2 = new Book(2, "Name", author, "Description", 17849102);
+            Author author2 = new Author("name", "surname", 1984);
+            Book a2 = new Book(1, "Name", author2, "Description", 17849102);
 
 
             Book[] books = {
-              //  new Book(1, "Name", author, "Description", 17849102),
+                new Book(1, "Name", author, "Description", 17849102),
+                new Book(1, "Name", author, "Description", 17849102),
             };
 
             Reader reader = new Reader("name", "surname", books);
 
             reader.Add(new Book(1, "Name", author, "Description", 17849102));
-            author.name = "Alexey";
+            //author.name = "Alexey";
             reader.Add(new Book(2, "Name2", author, "Description ", 17849101));
 
             
@@ -27,7 +29,8 @@ namespace ConsoleAplication
             Console.WriteLine(reader.ToString());
 
             Console.WriteLine("\n\n");
-            Console.WriteLine(a == a2);
+            Console.WriteLine("author == author2: ", author == author2);
+            Console.WriteLine("a == a2: ", a == a2);
         }
     }
 }
