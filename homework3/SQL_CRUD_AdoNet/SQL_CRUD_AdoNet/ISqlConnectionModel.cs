4944fe2efe95202c23d74810a20506459131db7b
Ivan Savin <68004563+SoV3n04ek@users.noreply.github.com>
2022-01-14 12:10:37 +0200

    added files SqlConnectionModel.cs and ISqlConnectionModel.cs

---
+++ homework3/SQL_CRUD_AdoNet/SQL_CRUD_AdoNet/ISqlConnectionModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+
+namespace SQL_CRUD_AdoNet
+{
+    interface ISqlConnectionModel
+    {
+        void Add();
+        void Edit(int id);
+        void Delete(int id);
+    }
+}
---
+++ homework3/SQL_CRUD_AdoNet/SQL_CRUD_AdoNet/SqlConnectionModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SQL_CRUD_AdoNet
+{
+    internal class SqlConnectionModel : ISqlConnectionModel
+    {
+        void ISqlConnectionModel.Add()
+        {
+
+        }
+
+        void ISqlConnectionModel.Delete(int id)
+        {
+
+        }
+
+        void ISqlConnectionModel.Edit(int id)
+        {
+
+        }
+    }
+}
